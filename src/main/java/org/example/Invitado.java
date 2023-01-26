0831ed434a71cbff356d796752d74304582827fa
308 <308@academicos.cesde.edu.co>
2023-01-26 12:08:11 -0500

    se crea proyecto fiestas

---
+++ src/main/java/org/example/Invitado.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Invitado {
+
+    //ATRIBUTOS=VARIABLES
+    int edad;
+    boolean sexo;
+    String nombre;
+    double valorEntrada;
+
+    //METODOS
+
+    //Metodos especiales
+
+    public Invitado() {
+    }
+
+    public Invitado(int dato1, boolean dato2, String dato3, double dato4) {
+        this.edad = dato1;
+        this.sexo = dato2;
+        this.nombre = dato3;
+        this.valorEntrada = dato4;
+    }
+
+
+
+    //Metodos ordinarios
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //EN EL MAIN LLAMO A MIS CLASES
+        //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
+        //EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
+        //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
+        Invitado objetoDeClaseInvitado=new Invitado();
+        Invitado objeto2=new Invitado(333,true,"Juan",10000);
+
+
+        //obtener el valor de los atributos
+        System.out.println(objetoDeClaseInvitado.nombre);
+        System.out.println(objeto2.nombre);
+
+    }
+}
